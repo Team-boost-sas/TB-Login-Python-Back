1f54f1b63871ab1bc2e22258d3dca0e229f6f82c
oscar98farfan <farfanjuaniasoscardaniel@gmail.com>
2023-06-24 14:23:07 -0500

    Creacion base de datos

---
+++ src/config/databases.py
@@ -0,0 +1,18 @@
+import os #Importamos SO
+
+from sqlalchemy import create_engine
+from sqlalchemy.orm.session import sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
+
+sqlite_file_name = "../database.sqlite" # Nombre BD
+base_dir = os.path.dirname(os.path.realpath(__file__)) # Direccion BD
+
+database_url = f"sqlite:///{os.path.join(base_dir,sqlite_file_name)}"
+
+engine = create_engine(database_url, echo=True)
+
+Session = sessionmaker(bind = engine)
+
+Base = declarative_base()
+
+# Nos vamos para models para crear mis tablas
---
+++ src/models/user.py
@@ -0,0 +1,16 @@
+from config.databases import Base
+from sqlalchemy import Column, Integer, String, Float
+
+
+class User(Base):
+    __tablename__ = 'users'
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    email = Column(String)
+    password = Column(String)
+    phone = Column(String)
+    secret = Column(String)
+    updated_at = Column(String)
+    created_at = Column(String)
+    status = Column(String)
+    status_email = Column(String)
+    status_phone = Column(String)
